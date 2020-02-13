959f68778d0d6273e9206eb3dcf3186f138ef33a
karmic <karmic.c64@gmail.com>
2020-02-13 03:12:19 -0700

    add license

--- nsfid.c
+++ nsfid.c
@@ -1,4 +1,19 @@
-
+/*  nsfid - Play routine/driver identifier for binary rips of computer music
+ *  Copyright (C) 2020 karmic <karmic.c64@gmail.com>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <unistd.h>
 #include <stdlib.h>
@@ -271,7 +286,7 @@ void scandir(const char* name)
         namlen = strlen(name);
     
     struct dirent* de;
-    while (de = readdir(dir))
+    while ((de = readdir(dir)))
     {
         char* fnam = de->d_name;
         if (!strcmp(fnam, ".") || !strcmp(fnam, "..")) continue;
